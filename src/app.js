dfc5f9aa28b64e67a9be5932d248061a854bb9cd
josehs08 <168867459+josehs08@users.noreply.github.com>
2024-06-01 21:33:14 +0000

    Primera version

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excusa").innerHTML = generarExcusa(who,action,what,when);
 };
+
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = ["before the class","when I was sleeping","while I was exercising","during my lunch","while I was praying"];
+
+function generarExcusa(who, action, what, when) {
+  let excusa = "";
+  excusa = excusa + who[Math.floor(Math.random() * 4)] + " ";
+  excusa = excusa + action[Math.floor(Math.random() * 4)] + " ";
+  excusa = excusa + what[Math.floor(Math.random() * 3)] + " ";
+  excusa = excusa + when[Math.floor(Math.random() * 5)] + " ";
+
+  return excusa;
+}
